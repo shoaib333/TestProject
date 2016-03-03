a6acf5dcc1d8e986d50203ee4cbca794445297a0
shoaib <shoaib.fst@hotmail.com>
2016-03-03 23:04:45 +0500

    First Commit with initial change

---
+++ app/src/main/java/com/example/dell/helloandroid/BackgroundServiceBTService.java
@@ -0,0 +1,79 @@
+package com.example.dell.helloandroid;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.ResultReceiver;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+/**
+ * Created by DELL on 3/3/2016.
+ */
+public class BackgroundServiceBTService extends Service {
+    public static final int STATUS_RUNNING = 0;
+    public static final int STATUS_FINISHED = 1;
+    public static final int STATUS_ERROR = 2;
+
+    //default constructor
+    public BackgroundServiceBTService() {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Context context = getApplicationContext();
+                CharSequence text = "Bluetooth is not enabled";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                // Bluetooth is not enable :)
+            } else {
+                Context context = getApplicationContext();
+                CharSequence text = "Bluetooth is Enabled";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                // Bluetooth is enable
+            }
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+
+}
