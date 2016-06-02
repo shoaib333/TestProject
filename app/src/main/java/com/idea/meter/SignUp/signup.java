5af58ebf2e9b6f1d3fe83df769bdb70a44e71864
Salman Ahmed <salman03444@yahoo.com>
2016-06-02 22:54:05 +0500

    Refactored code onCreat sy nikal diya hai :P

--- app/src/main/java/com/idea/meter/SignUp/signup.java
+++ app/src/main/java/com/idea/meter/SignUp/signup.java
@@ -20,18 +20,21 @@ import com.idea.meter.Startup_Pages.login_page;
 
 public class signup extends login_page {
     public static final String PREFS_NAME = "MyPrefsFile";
-
+    Button app_signup;
+    EditText User_Name;
+    EditText Password;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_signup);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final Button app_signup = (Button) findViewById(R.id.signup_create);
-        final EditText User_Name = (EditText) findViewById(R.id.new_user_name);
-        final EditText Password = (EditText) findViewById(R.id.new_password);
+        app_signup = (Button) findViewById(R.id.signup_create);
+        User_Name = (EditText) findViewById(R.id.new_user_name);
+        Password = (EditText) findViewById(R.id.new_password);
 
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
 
@@ -40,20 +43,30 @@ public class signup extends login_page {
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(fabListener);
 
         app_signup.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                try {
-                    String u_n = User_Name.getText().toString();
-                    String pw = Password.getText().toString();
+
+            }
+        });
+    }
+
+
+    View.OnClickListener fabListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    };
+    View.OnClickListener signupListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            try {
+                String u_n = User_Name.getText().toString();
+                String pw = Password.getText().toString();
 
                     if (User_Name.getText().toString().equals("") || Password.getText().toString().equals("")) {
                         AlertDialog.Builder dlgAlert = new AlertDialog.Builder(signup.this);
@@ -68,43 +81,52 @@ public class signup extends login_page {
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int which) {
 
+                                }
+                            });
+                } else {
+                    List<Contact> contacts = database.getAllContacts();
+                    boolean found_flag = false;
+
+                    for (Contact cn : contacts) {
+                        String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: " + cn.getPhoneNumber();
+                        // Writing Contacts to log
+                        Log.d("Name: ", log);
+                        if(User_Name.getText().toString().equals(cn.getName()))
+                        {
+                            found_flag = true;
+                            break;
+                        }
+                    }
+                    if (found_flag == false) {
+                        database.addContact(new Contact(u_n, pw));
+                        Intent getIntent = new Intent(signup.this, login_page.class);
+                        getIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(getIntent);
+                        finish(); // call this to finish the current activity
+                    }
+                    else
+                    {
+                        AlertDialog.Builder dlgAlert = new AlertDialog.Builder(signup.this);
+
+                        dlgAlert.setMessage("Choose another username");
+                        dlgAlert.setTitle("Username already exists");
+                        dlgAlert.setPositiveButton("OK", null);
+                        dlgAlert.setCancelable(true);
+                        dlgAlert.create().show();
+
+                        dlgAlert.setPositiveButton("Ok",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+
                                     }
                                 });
-                    } else {
-
-                        Boolean temp = true;//TODO will be removed when http request is adde
-                        // We need an Editor object to make preference changes.
-                        // All objects are from android.context.Context
-                        Settings appSettings = new Settings(getApplicationContext());
-                        appSettings.InitializeSharedPreferences(getApplicationContext());
-
-                        //TODO: make a http GET call to server for signup
-                        //and handle the response
-                        //if it is successful move to next page otherwise
-                        //prompt the user for forget password as already registered
-                        appSettings.setPassword(pw);
-                        appSettings.setName(u_n);
-
-                        Toast.makeText(getApplicationContext(), "Name = :"+appSettings.getName()+
-                                " pswd : "+appSettings.getPassword(), Toast.LENGTH_LONG).show();
-
-
-                        if (temp)
-                        {
-                            Intent getIntent = new Intent(signup.this, login_page.class);
-                            getIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(getIntent);
-                            finish(); // call this to finish the current activity
-                        }
-
                     }
-
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Exception Caught : "+
-                            e.getMessage().toString(), Toast.LENGTH_LONG).show();
                 }
+
+            } catch (Exception e) {
+
             }
-        });
-    }
+        }
+    };
 
 }
