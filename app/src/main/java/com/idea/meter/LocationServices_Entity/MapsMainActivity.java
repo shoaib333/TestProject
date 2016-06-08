2b1a989a10275573a2d9cfb7be74aabd6116206b
osamasalahuddin <osama.salahuddin@gmail.com>
2016-06-08 23:54:06 +0500

    Permissions Corrected for Android Marshmallow and greater versions

--- app/src/main/java/com/idea/meter/LocationServices_Entity/MapsMainActivity.java
+++ app/src/main/java/com/idea/meter/LocationServices_Entity/MapsMainActivity.java
@@ -1,12 +1,16 @@
 package com.idea.meter.LocationServices_Entity;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
@@ -20,7 +24,6 @@ import android.widget.Toast;
 import com.idea.meter.Main_Entity.MainActivity;
 import com.idea.meter.R;
 import com.idea.meter.SMS_Entity.SmsBroadcastReceiver;
-import com.idea.meter.Startup_Pages.about_idealojy;
 import com.idea.meter.Startup_Pages.login_page;
 
 public class MapsMainActivity extends AppCompatActivity {
@@ -43,6 +46,13 @@ public class MapsMainActivity extends AppCompatActivity {
     private Location myBestReading;
     private LocationManager myLocationManager;
     private LocationListener myLocationListener;
+
+    /* Request Codes for Permissions */
+    final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    final int MY_PERMISSIONS_REQUEST_READ_SMS = 2;
+    final int MY_PERMISSIONS_REQUEST_BLUETOOTH = 3;
+    final int MY_PERMISSIONS_REQUEST_BLUETOOTH_MANAGER = 4;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -64,7 +74,7 @@ public class MapsMainActivity extends AppCompatActivity {
         sms_button = (Button) findViewById(R.id.smsButton);
         smsBroadcastReceiver = new SmsBroadcastReceiver();
 
-        toolbar_main.setTitle("IdeaMeter");
+        toolbar_main.setTitle("ideaMeter");
         toolbar_main.inflateMenu(R.menu.personal_screen);
 
         toolbar_main.setOnMenuItemClickListener(toolListener);
@@ -109,11 +119,39 @@ public class MapsMainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             try{
+                /* Check if Marshmallow or greater */
+                if (Build.VERSION.SDK_INT >= 23){
+
+                    /* Check if Permission is not available */
+                    if (ContextCompat.checkSelfPermission(MapsMainActivity.this,
+                            Manifest.permission.READ_SMS)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        /* TODO: To display custom dialogue box this API call cant be used
+                        *  see more details here: https://developer.android.com/training/permissions/requesting.html
+                        */
+
+                        /* Request the permission */
+
+                        ActivityCompat.requestPermissions(MapsMainActivity.this,
+                                new String[]{Manifest.permission.READ_SMS},
+                                MY_PERMISSIONS_REQUEST_READ_SMS);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+
+                    }
+                }
+
+
                 Intent getIntent = new Intent(MapsMainActivity.this, SmsBroadcastReceiver.class);
-                    /* Register SmsBroadcast Receiver to Read SMS */
+
+                /* Register SmsBroadcast Receiver to Read SMS */
                 registerReceiver(smsBroadcastReceiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
 
                 Toast.makeText(getBaseContext(), "SMS Reading On", Toast.LENGTH_SHORT).show();
+
             } catch (Exception e){
 
             }
@@ -124,7 +162,56 @@ public class MapsMainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             try {
+
+             /* Check if Marshmallow or greater */
+                if (Build.VERSION.SDK_INT >= 23){
+
+                    /* Check if Permission is not available */
+                    if (ContextCompat.checkSelfPermission(MapsMainActivity.this,
+                            Manifest.permission.BLUETOOTH)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        /* TODO: To display custom dialogue box this API call cant be used
+                        *  see more details here: https://developer.android.com/training/permissions/requesting.html
+                        */
+
+                        /* Request the permission */
+
+                        ActivityCompat.requestPermissions(MapsMainActivity.this,
+                                new String[]{Manifest.permission.BLUETOOTH},
+                                MY_PERMISSIONS_REQUEST_BLUETOOTH);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+
+                    }
+
+                    /* Check if Permission is not available */
+                    if (ContextCompat.checkSelfPermission(MapsMainActivity.this,
+                            Manifest.permission.BLUETOOTH_ADMIN)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        /* TODO: To display custom dialogue box this API call cant be used
+                        *  see more details here: https://developer.android.com/training/permissions/requesting.html
+                        */
+
+                        /* Request the permission */
+
+                        ActivityCompat.requestPermissions(MapsMainActivity.this,
+                                new String[]{Manifest.permission.BLUETOOTH_ADMIN},
+                                MY_PERMISSIONS_REQUEST_BLUETOOTH);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+
+                    }
+
+                }
+
                 Intent getIntent = new Intent(MapsMainActivity.this, MainActivity.class);
+
                 startActivity(getIntent);
             } catch (Exception e) {
 
@@ -154,5 +241,91 @@ public class MapsMainActivity extends AppCompatActivity {
         }
     };
 
-}
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_SMS: {
+                /* If request is cancelled, the result arrays are empty. */
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+
+                    /* permission was granted, yay! Do the
+                     * task you need to do.
+                     */
+                } else {
+
+                    /* permission denied, Disable the
+                     * functionality that depends on this permission.
+                     */
+                    Toast.makeText(getBaseContext(), "Permission not Granted for Reading of SMS", Toast.LENGTH_SHORT).show();
+
+                }
+                return;
+            }
+
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                /* If request is cancelled, the result arrays are empty. */
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    /* permission was granted, yay! Do the
+                     * contacts-related task you need to do.
+                     */
+                } else {
+
+                    /* permission denied, Disable the
+                     * functionality that depends on this permission.
+                     */
+                    Toast.makeText(getBaseContext(), "Permission not Granted for Contacts Reading", Toast.LENGTH_SHORT).show();
+
+                }
+                return;
+            }
+
+            case MY_PERMISSIONS_REQUEST_BLUETOOTH: {
+                /* If request is cancelled, the result arrays are empty. */
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    /* permission was granted, yay! Do the
+                     * contacts-related task you need to do.
+                     */
+                } else {
+
+                    /* permission denied, Disable the
+                     * functionality that depends on this permission.
+                     */
+                    Toast.makeText(getBaseContext(), "Permission not Granted for Bluetooth", Toast.LENGTH_SHORT).show();
+
+                }
+                return;
+            }
+
+            case MY_PERMISSIONS_REQUEST_BLUETOOTH_MANAGER: {
+                /* If request is cancelled, the result arrays are empty. */
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    /* permission was granted, yay! Do the
+                     * contacts-related task you need to do.
+                     */
+                } else {
+
+                    /* permission denied, Disable the
+                     * functionality that depends on this permission.
+                     */
+                    Toast.makeText(getBaseContext(), "Permission not Granted for Bluetooth Admin", Toast.LENGTH_SHORT).show();
+
+                }
+                return;
+            }
+
+            /* other 'case' lines to check for other
+             * permissions this app might request
+             */
+        }
+    }
+
+}
